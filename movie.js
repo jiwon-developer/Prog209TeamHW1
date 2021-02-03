2055119b2814120a6d192c61c0a13cf8f2a855e5
Michelle Guerrero <michelleguerrero@Michelles-MacBook-Pro.local>
2021-02-02 22:03:46 -0800

    movie js updates

--- movie.js
+++ movie.js
@@ -1,3 +1,14 @@
 function Test() {
     alert("Submit a review!");
 }
+
+
+function Movie(title, year, prating) {
+    this.title = title;
+    this.year = year;
+    this.prating = prating;
+}
+
+function Rating(rating) {
+    this.rating = rating;
+}
