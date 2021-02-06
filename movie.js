8e03f8c09c20cf2b6ce51ce58b09eaa2299d28aa
Michelle Guerrero <michelleguerrero@Michelles-MacBook-Pro.local>
2021-02-05 23:27:41 -0800

    updates

--- movie.js
+++ movie.js
@@ -1,13 +1,28 @@
-class Movie {
-    constructor(name, rating) {
-        this.name = name;
-        this.rating = rating;
+let Movie = function(title, rating) {
+    this.title = title;
+    this.rating = rating;
+    this.isValid = function() {}
+    this.toString = function() {
+
+    };
+};
+
+
+
+function Movie() {
+
+    if (isNaN(rating) || x < 1 || x > 5) {
+        text = "Input not valid. Please rate movie from 1 to 5.";
     }
-}
+};
 
 movie.prototype.toString = function movietoString() {
     return "Movie title: " + this.name + "Movie Rating: " + this.rating
 };
 
-let movieName = document.getElementById("name");
-let movieRating = document.getElementById("rating");
+
+
+let movie = document.getElementById("movie");
+let rating = document.getElementById("rating");
+
+console.log(Movie.toString());
